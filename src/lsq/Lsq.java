34923f1357c4457f40baefec05e654d5fba6f9a9
corganfuzz <corganfuzz@localhost.localdomain>
2020-11-01 23:07:17 -0600

    finished

--- src/lsq/Lsq.java
+++ src/lsq/Lsq.java
@@ -2,18 +2,15 @@ package lsq;
 
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Lsq {
 
     static void userSelection() throws Exception {
-        System.out.println("\n======BASIC OPTIONS=======");
+        System.out.println("\n======BASIC OPTIONS=======\n");
         Scanner input = new Scanner(System.in);
         System.out.println("1. Reverse \n2. Convert \n3. Compare");
-        System.out.print("\nPlease select an option: ");
+        System.out.print("\nMake your selection: ");
         int selection = input.nextInt();
         System.out.println("\nThe selected file is states.txt");
 
@@ -24,16 +21,11 @@ public class Lsq {
             System.out.println("The file contents have been dequeue and converted");
             dequeueUpperLinked();
         } else if (selection == 3) {
-            System.out.println("Compare function");
+            compareQ();
         } else {
             System.out.println("Invalid Option only 1, 2 or 3 allowed");
         }
 
-    }
-
-    static String readTextFile() throws Exception {
-        Scanner sc = new Scanner(new FileReader("states.txt"));
-        return sc.nextLine();
     }
 
     static void writeToFile(String data, String filename) {
@@ -49,8 +41,10 @@ public class Lsq {
 
     static void dequeueUpperLinked() throws Exception {
         String queueSavedFile = "queueUpper.txt";
-        String stringFromFile = readTextFile();
+        String stringFromFile = readTextFile("states.txt");
+
         Queue<Character> myQ = new LinkedList<>();
+
         for(char c: stringFromFile.toCharArray()) {
             myQ.add(c);
         }
@@ -62,13 +56,11 @@ public class Lsq {
         }
         writeToFile(qOutput.toUpperCase(), queueSavedFile);
         System.out.println("\nThis content have been written to " + queueSavedFile + ", located at the root folder");
-
-
     }
 
     static void reversePopStack() throws Exception {
         String reversedSavedFile = "reversedStack.txt";
-        String stringFromFile = readTextFile();
+        String stringFromFile = readTextFile("states.txt");
         Stack<Character> myStack = new Stack<>();
         for(char c: stringFromFile.toCharArray()) {
             myStack.push(c);
@@ -80,6 +72,42 @@ public class Lsq {
         }
         writeToFile(outputChar, reversedSavedFile);
         System.out.println("\nThis content have been written to " + reversedSavedFile + ", located at the root folder");
+    }
+
+    static String readTextFile(String filename) throws Exception {
+        Scanner sc = new Scanner(new FileReader(filename));
+        return sc.nextLine();
+    }
+
+    static void compareQ() throws Exception {
+        String stringOne = readTextFile("reversedStack.txt");
+        String stringTwo = readTextFile("queueUpper.txt");
+
+        PriorityQueue<Character> firstQ = new PriorityQueue<>();
+        for (char c: stringOne.toCharArray()) {
+            firstQ.add(c);
+        }
+
+        System.out.println("The first Queue is: " + firstQ);
+
+        PriorityQueue<Character> secondQ = new PriorityQueue<>();
+        for (char c: stringTwo.toCharArray()) {
+            secondQ.add(c);
+        }
+
+        System.out.println("The second Queue is: " + secondQ);
+
+        if(firstQ.equals(secondQ)) {
+            System.out.println("The Queues are identical");
+        } else {
+            System.out.println("""
+
+                    THE QUEUES ARE NOT THE SAME
+                    Yes and No indicate identical queue elements""");
+            for (Character element : firstQ) {
+                System.out.println(secondQ.contains(element) ? "Yes" : "No");
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
